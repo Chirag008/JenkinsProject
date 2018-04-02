25073682f0349411de5c7bade6f165407056c677
Chirag Bansal <chirag.bansal17@gmail.com>
2018-04-02 09:40:18 +0530

    Updated Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello{
 
 	public static void main(String[] args){
-		System.out.println("hello world Updated For demo")
+		System.out.println("hello world Updated For demo");
+		System.out.println("I am exiting !");
 	
 	}
 
